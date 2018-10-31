bb7d6d7061e8f7b67562431f73a1d9270bf95cbc
Sara Chronister <32340866+sarachronister@users.noreply.github.com>
2018-10-31 13:58:59 -0700

    Update and rename

--- Extract_Clean/6 - Data frames for plots and tables.R
+++ Extract_Clean/6 - Data frames for plots and tables.R
@@ -30,3 +30,19 @@ visit_per_event_by_file <- cum_update_visit %>%
  
 visit_total_by_file <- fill_titles_lines_visit %>%
   mutate(Arrived_Date_CDT = as.Date(Arrived_Date_CDT))
+
+## Table of message and visit counts below threshold values ############################################################################################################################################
+
+message_new <- message_per_event_by_file %>%
+  filter(Recorded_Date > as.Date(plot_earliest_date)) %>%
+  filter(Recorded_Date < Sys.Date()) %>%
+  group_by(Parent_Organization, Recorded_Date, message_threshold_line) %>%
+  summarise(cum_messages = max(cum_messages)) %>%
+  mutate(message_low = ifelse(cum_messages < message_threshold_line,1,0))
+
+visit_new <- visit_per_event_by_file %>%
+  filter(C_Visit_Date >= as.Date(plot_earliest_date)) %>%
+  filter(C_Visit_Date < Sys.Date()) %>%
+  group_by(Parent_Organization, C_Visit_Date, visit_threshold_line) %>%
+  summarise(cum_visits = max(cum_visits)) %>%
+  mutate(visit_low = ifelse(cum_visits < visit_threshold_line,1,0))
