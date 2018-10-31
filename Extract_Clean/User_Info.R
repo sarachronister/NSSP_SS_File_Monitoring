803eb8a3d522a5b24858482b61ecef58f57a34a1
Sara Chronister <32340866+sarachronister@users.noreply.github.com>
2018-10-31 10:25:28 -0700

    Create User_Info.R

---
+++ Extract_Clean/User_Info.R
@@ -0,0 +1,7 @@
+# Replace the *********** with your BioSense username and password in the appropriate lines
+# Update when you need to change your password
+# Save to your home directory "~/"
+
+password <- dput(charToRaw("***********"))
+UserID <- dput(charToRaw("***********"))
+username <- dput(charToRaw("BIOSENSE\\***********"))
